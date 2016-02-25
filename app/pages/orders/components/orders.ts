7f88f926848e3608094637b7ad14336478172564
JJ Jiles <jj@jjis.me>
2016-02-25 15:24:29 -0600

    First initial commit. Added orders page

---
+++ app/pages/orders/components/orders.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'orders',
+  templateUrl: './pages/orders/components/orders.html',
+  styleUrls: ['./pages/orders/components/orders.css']
+})
+export class OrdersPage {
+}
