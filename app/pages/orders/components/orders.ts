fecc619a567f7418b064f1cc44482177f82b8beb
JJ Jiles <jj@jjis.me>
2016-02-26 01:27:20 -0600

    added the orders componet, view and service

--- app/pages/orders/components/orders.ts
+++ app/pages/orders/components/orders.ts
@@ -1,9 +1,14 @@
 import {Component} from 'angular2/core';
 
+import {OrderListService} from '../../../shared/services/orders-list.service';
+
 @Component({
   selector: 'orders',
   templateUrl: './pages/orders/components/orders.html',
-  styleUrls: ['./pages/orders/components/orders.css']
+  styleUrls: ['./pages/orders/components/orders.css'],
+  bindings: [OrderListService]
 })
 export class OrdersPage {
+    constructor(public orderListService:OrderListService) {
+    }
 }
---
+++ app/shared/services/orders-list.service.ts
@@ -0,0 +1,154 @@
+export class OrderListService {
+  orders = [
+    {
+      'orderNumber': '10334555',
+      'datePlaced': '8/30/2012 2:31 PM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$129.99'
+    },{
+      'orderNumber': '1345955',
+      'datePlaced': '12/20/2011 8:57 AM',
+      'location': 'Broken Arrow, OK',
+      'name': 'Hank Haines',
+      'amount': '$9.99'
+    },{
+      'orderNumber': '1456835',
+      'datePlaced': '1/24/2011 9:35 AM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$34.99'
+    },{
+      'orderNumber': '17624667',
+      'datePlaced': '11/15/2010 9:48 AM',
+      'location': 'Oklahoma City, OK',
+      'name': 'Hank Haines',
+      'amount': '$19.99'
+    },{
+      'orderNumber': '1789453',
+      'datePlaced': '7/6/2010 1:40 PM',
+      'location': 'Jenks, OK',
+      'name': 'Hank Haines',
+      'amount': '$145.99'
+    },{
+      'orderNumber': '1030955',
+      'datePlaced': '6/20/2010 8:57 AM',
+      'location': 'Sapulpa, OK',
+      'name': 'Hank Haines',
+      'amount': '$1099.99'
+    },
+    {
+      'orderNumber': '10334555',
+      'datePlaced': '8/30/2012 2:31 PM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$129.99'
+    },{
+      'orderNumber': '1345955',
+      'datePlaced': '12/20/2011 8:57 AM',
+      'location': 'Broken Arrow, OK',
+      'name': 'Hank Haines',
+      'amount': '$9.99'
+    },{
+      'orderNumber': '1456835',
+      'datePlaced': '1/24/2011 9:35 AM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$34.99'
+    },{
+      'orderNumber': '17624667',
+      'datePlaced': '11/15/2010 9:48 AM',
+      'location': 'Oklahoma City, OK',
+      'name': 'Hank Haines',
+      'amount': '$19.99'
+    },{
+      'orderNumber': '1789453',
+      'datePlaced': '7/6/2010 1:40 PM',
+      'location': 'Jenks, OK',
+      'name': 'Hank Haines',
+      'amount': '$145.99'
+    },{
+      'orderNumber': '1030955',
+      'datePlaced': '6/20/2010 8:57 AM',
+      'location': 'Sapulpa, OK',
+      'name': 'Hank Haines',
+      'amount': '$1099.99'
+    },
+    {
+      'orderNumber': '10334555',
+      'datePlaced': '8/30/2012 2:31 PM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$129.99'
+    },{
+      'orderNumber': '1345955',
+      'datePlaced': '12/20/2011 8:57 AM',
+      'location': 'Broken Arrow, OK',
+      'name': 'Hank Haines',
+      'amount': '$9.99'
+    },{
+      'orderNumber': '1456835',
+      'datePlaced': '1/24/2011 9:35 AM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$34.99'
+    },{
+      'orderNumber': '17624667',
+      'datePlaced': '11/15/2010 9:48 AM',
+      'location': 'Oklahoma City, OK',
+      'name': 'Hank Haines',
+      'amount': '$19.99'
+    },{
+      'orderNumber': '1789453',
+      'datePlaced': '7/6/2010 1:40 PM',
+      'location': 'Jenks, OK',
+      'name': 'Hank Haines',
+      'amount': '$145.99'
+    },{
+      'orderNumber': '1030955',
+      'datePlaced': '6/20/2010 8:57 AM',
+      'location': 'Sapulpa, OK',
+      'name': 'Hank Haines',
+      'amount': '$1099.99'
+    },
+    {
+      'orderNumber': '10334555',
+      'datePlaced': '8/30/2012 2:31 PM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$129.99'
+    },{
+      'orderNumber': '1345955',
+      'datePlaced': '12/20/2011 8:57 AM',
+      'location': 'Broken Arrow, OK',
+      'name': 'Hank Haines',
+      'amount': '$9.99'
+    },{
+      'orderNumber': '1456835',
+      'datePlaced': '1/24/2011 9:35 AM',
+      'location': 'Tulsa, OK',
+      'name': 'Hank Haines',
+      'amount': '$34.99'
+    },{
+      'orderNumber': '17624667',
+      'datePlaced': '11/15/2010 9:48 AM',
+      'location': 'Oklahoma City, OK',
+      'name': 'Hank Haines',
+      'amount': '$19.99'
+    },{
+      'orderNumber': '1789453',
+      'datePlaced': '7/6/2010 1:40 PM',
+      'location': 'Jenks, OK',
+      'name': 'Hank Haines',
+      'amount': '$145.99'
+    },{
+      'orderNumber': '1030955',
+      'datePlaced': '6/20/2010 8:57 AM',
+      'location': 'Sapulpa, OK',
+      'name': 'Hank Haines',
+      'amount': '$1099.99'
+    }];
+  get(): Array<any> {
+    return this.orders;
+  }
+}
